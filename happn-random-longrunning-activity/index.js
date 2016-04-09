3b997f2eda9cc22e7adda29f8c327413cfe56fe7
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2016-04-09 13:57:07 +0200

    have tests ready for remote installation

--- happn-random-longrunning-activity/index.js
+++ happn-random-longrunning-activity/index.js
@@ -6,15 +6,53 @@ var spawn = require('child_process').spawn
   , happn_client = happn.client
   ;
 
+var clientInstance;
+
+var clientConfig = {
+	config:{
+	  secure:true,
+	  username:'_ADMIN',
+	  password:'happn'
+	}
+}
+
+var ACTIVITY_LOG_INTERVAL = 5000;
+
 // spawn remote mesh in another process
 remote = spawn('node', [__dirname + '/happn_instance']);
+
+var random;
 
 remote.stdout.on('data', function(data) {
 
   console.log(data.toString());
 
   if (data.toString().match(/READY/)){
-   console.log('READY - TEST FRAMEWORK UP:::');
+  	happn_client.create(clientConfig, function(e, instance){
+  		if (e){
+  			console.log('error connecting to remote happn instance: ', e);
+  			return endProcess();
+  		}
+  		clientInstance = instance;
+  		var RandomActivityGenerator = require("happn-random-activity-generator");
+  		random = new RandomActivityGenerator(clientInstance);
+
+  		random.generateActivityStart("test", function(e){
+
+  			if (e){
+	  			console.log('error starting random activity: ', e);
+	  			return endProcess();
+	  		}
+
+	  		setInterval(function(){
+	  			var aggregatedLog = random.__operationLogAggregated["test"];
+	  			console.log('RANDOM ACTIVITIES - AGGREGATED:::', aggregatedLog);
+	  		}, ACTIVITY_LOG_INTERVAL);
+
+	  		console.log('READY - TEST FRAMEWORK UP:::');
+  		}, "daemon")
+
+  	})
   }
 
   if (data.toString().indexOf(/DUMP/) >= 0){
@@ -31,15 +69,30 @@ remote.stdout.on('data', function(data) {
 
 var endProcess = function(){
 	try{
-		if (remote){
-			console.log('killing remote process:::');
-			remote.kill();
-			console.log('remote process killed:::');
+
+		var killRemote = function(){
+			if (remote){
+				console.log('killing remote process:::');
+				remote.kill();
+				console.log('remote process killed:::');
+			}
+			process.exit(0);
 		}
+
+		if (random)
+			random.generateActivityEnd("test", function(aggregatedLog){
+				console.log('ended random activity:::', aggregatedLog);
+				killRemote();
+			});
+		else
+			killRemote();
+
+
 	 }catch(e){
 	 	//do nothing
+	 	process.exit(0);
 	 }
-	 process.exit(0);
+
 
 };
 
